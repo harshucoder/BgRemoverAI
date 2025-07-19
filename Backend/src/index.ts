d2f88ec0224645d3ffc3ab8a5da9dee7e7943f8f
Harsh Kushwaha <128130770+harshucoder@users.noreply.github.com>
2025-07-19 19:17:39 +0530

    Fixed: Cors Error

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,21 +1,32 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
 import removeBgRouter from "./routes/removeBg";
 
 const app = express();
 const PORT = 5000;
-
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://https://bg-remover-ai-ten.vercel.app/",
+];
 app.use((req, res, next) => {
   console.log(`Request URL: ${req.method} ${req.url}`);
   next();
 });
 
-app.use(cors());
+app.use(cors({
+  origin: function (origin, callback){
+    if(!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    }else{
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 
 // Mount your upload route BEFORE express.json()
 app.use("/removebg", removeBgRouter);
 
-app.use(express.json());  // for other routes that accept json
+app.use(express.json()); // for other routes that accept json
 
 app.listen(PORT, () => {
   console.log(`server is running on the PORT ${PORT}`);
